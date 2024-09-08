e64eb03482fafd36489f84644766e2d3835ad331
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-08 09:58:28 -0600

    Update product.router.js

--- src/routes/v1/product.router.js
+++ src/routes/v1/product.router.js
@@ -7,52 +7,56 @@ const {
   getMyProducts,
   updateProductImages,
   deleteProductImages,
-} = require('../../controllers/v1/product.controller');
-const express = require('express');
-const upload = require('../../utils/multer');
-const { protect, restrictTo } = require('../../middlewares/auth.middleware');
+  getProductsFiltered,
+} = require("../../controllers/v1/product.controller");
+const express = require("express");
+const upload = require("../../utils/multer");
+const { protect, restrictTo } = require("../../middlewares/auth.middleware");
 const {
   validProductPerFindOne,
   validProductUser,
   validProductAdmin,
-} = require('../../middlewares/product.middleware');
+} = require("../../middlewares/product.middleware");
 const {
   dataProductValidation,
   deleteProductImagesValidation,
   filterProducstValidation,
-} = require('../../middlewares/validations.middleware');
+} = require("../../middlewares/validations.middleware");
 
 const productRouter = express.Router();
 
-productRouter.route('/').get(filterProducstValidation, getProducts).post(
-  protect,
-  upload.array('productImgs', 5),
-  dataProductValidation,
+productRouter
+  .route("/")
+  .get(filterProducstValidation, getMyProducts)
+  .post(
+    protect,
+    upload.array("productImgs", 5),
+    dataProductValidation,
+    createProduct
+  );
 
-  createProduct
-);
-
-productRouter.route('/me').get(protect, getMyProducts);
+productRouter.route("/search").get(getProductsFiltered);
+productRouter.route("/me").get(protect, getMyProducts);
 
 productRouter
-  .use('/me/:id', protect, validProductUser)
-  .route('/me/:id')
+  .use("/me/:id", protect, validProductUser)
+  .route("/me/:id")
   .delete(removeProduct)
   .patch(dataProductValidation, updateProduct);
 
 productRouter
-  .use('/me/:id/images', protect, validProductUser)
-  .route('/me/:id/images')
-  .patch(upload.array('productImgs', 5), updateProductImages)
+  .use("/me/:id/images", protect, validProductUser)
+  .route("/me/:id/images")
+  .patch(upload.array("productImgs", 5), updateProductImages)
   .delete(deleteProductImagesValidation, deleteProductImages);
 
 productRouter
-  .route('/:id')
+  .route("/:id")
   .get(validProductPerFindOne, getProduct)
-  .delete(protect, restrictTo('admin'), validProductAdmin, removeProduct)
+  .delete(protect, restrictTo("admin"), validProductAdmin, removeProduct)
   .patch(
     protect,
-    restrictTo('admin'),
+    restrictTo("admin"),
     validProductAdmin,
     dataProductValidation,
     updateProduct
