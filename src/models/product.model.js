b28ed11928ac8a1a54317ecc223439fb6e006d3f
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-13 05:17:08 -0600

    Update product.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,283 +1,86 @@
-const catchError = require("../../utils/catchError");
+const { DataTypes } = require("sequelize");
+const { db } = require("../database/config");
+const ProductCart = require("./productCart.model");
 
-const Product = require("../../models/product.model");
-const ProductImg = require("../../models/productImg.model");
-const { UploadFile } = require("../../utils/firebase-image-cloud");
-const { Op, Sequelize } = require("sequelize");
-const AppError = require("../../utils/appError");
-const Category = require("../../models/category.model");
-const Rating = require("../../models/rating.model");
-const obfuscateName = require("../../utils/ofuscName");
-const { db } = require("../../database/config");
-
-const getMyProducts = catchError(async (req, res) => {
-  const { id: userId } = req.sessionUser;
-  const products = await Product.findAll({
-    where: { userId, [Op.or]: [{ status: "active" }, { status: "pause" }] },
-    include: [{ model: ProductImg, attributes: ["id", "productImgUrl"] }],
-  });
-  return res.json({
-    status: "success",
-    message: "products successfully brought",
-    results: products.length,
-    products,
-  });
+const Product = db.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING(500),
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      min: 1.0,
+    },
+  },
+  brand: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  availableQuantity: {
+    type: DataTypes.INTEGER,
+    validate: {
+      min: 0,
+    },
+    allowNull: false,
+  },
+  sales: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  status: {
+    type: DataTypes.ENUM("active", "inactive", "pause"),
+    allowNull: false,
+    defaultValue: "active",
+  },
 });
 
-const getProducts = catchError(async (req, res) => {
-  const best_sellers = await Product.findAll({
-    where: { status: "active" },
-    include: [
-      {
-        model: ProductImg,
-        attributes: ["id", "productImgUrl"],
-        where: { status: "active" },
-        required: false,
-      },
-      {
-        model: Category,
-        attributes: ["id", "name"],
-      },
-      {
-        model: Rating,
-        attributes: ["id", "rating", "comment"],
-      },
-    ],
-    attributes: [
-      "id",
-      "title",
-      "description",
-      "price",
-      "availableQuantity",
-      "sales",
-    ],
-    order: [["sales", "desc"]],
-    limit: 8,
-  });
-
-  const best_rated = await Product.findAll({
-    where: { status: "active" },
-    include: [
-      {
-        model: ProductImg,
-        attributes: ["id", "productImgUrl"],
-        where: { status: "active" },
-        required: false,
-      },
-      {
-        model: Category,
-        attributes: ["id", "name"],
-      },
-      {
-        model: Rating,
-        attributes: ["id", "comment", "rating"],
-      },
-    ],
-    attributes: [
-      "id",
-      "title",
-      "description",
-      "price",
-      "availableQuantity",
-      "sales",
-    ],
-    group: ["product.id", "productImgs.id", "category.id", "ratings.id"],
-    order: [
-      [db.literal("AVG(ratings.rating) IS NULL"), "ASC"],
-      [db.fn("AVG", db.col("ratings.rating")), "DESC"],
-    ],
-    limit: 8,
-    subQuery: false,
-  });
-
-  return res.json({
-    status: "success",
-    message: "products successfully brought",
-    best_sellers,
-    best_rated,
-  });
-});
-
-const getProductsFiltered = catchError(async (req, res) => {
-  const { categories, title, min_price, max_price, short } = req.query;
-  let where = { status: "active" };
-  let whereCategories = {};
-  let order = [];
-
-  if (min_price) where.price = { [Op.gte]: min_price };
-  if (max_price) where.price = { ...where.price, [Op.lte]: max_price };
-  if (title) where.title = { [Op.iLike]: `%${title}%` };
-  if (short === "price-highest-first") order.push(["price", "DESC"]);
-  if (short === "price-lowest-first") order.push(["price", "ASC"]);
-  if (categories) whereCategories.name = { [Op.in]: categories.split(",") };
-
-  const products = await Product.findAll({
-    where: where,
-    attributes: [
-      "id",
-      "title",
-      "description",
-      "price",
-      "availableQuantity",
-      "sales",
-    ],
-    include: [
-      {
-        model: ProductImg,
-        attributes: ["id", "productImgUrl"],
-        where: { status: "active" },
-        required: false,
-      },
-      {
-        model: Category,
-        attributes: ["id", "name"],
-        where: whereCategories,
-      },
-      {
-        model: Rating,
-        attributes: ["id", "rating"],
-      },
-    ],
-    order,
-  });
-  return res.json({
-    status: "success",
-    message: "products successfully brought",
-    results: products.length,
-    products,
-  });
-});
-
-const getProduct = catchError(async (req, res) => {
-  const { product } = req;
-
-  product.ratings.forEach(({ user }) => {
-    user.firstName = obfuscateName(user.firstName);
-  });
-
-  return res.json({
-    status: "success",
-    message: "product brought successfully",
-    product,
-  });
-});
-
-const createProduct = catchError(async (req, res) => {
-  const { sessionUser, files } = req;
-  const { title, description, price, brand, availableQuantity, categoryId } =
-    req.body;
-
-  const product = await Product.create({
-    title,
-    description,
-    price,
-    brand,
-    categoryId,
-    availableQuantity,
-    userId: sessionUser.id,
-  });
-
-  if (files.length > 0) {
-    const productImgsPromises = files.map(async (file) => {
-      const imgUrl = await UploadFile.uploadToFirebase(
-        `products/${product.id}`,
-        file
-      );
-      return await ProductImg.create({
-        productId: product.id,
-        productImgUrl: imgUrl,
-      });
-    });
-    await Promise.all(productImgsPromises);
+Product.addHook("beforeSave", async (product, options) => {
+  if (product.availableQuantity === 0) {
+    product.status = "pause";
+  } else if (product.status === "pause") {
+    product.status = "active";
   }
-
-  const result = await Product.findByPk(product.id, {
-    include: [{ model: ProductImg, attributes: ["id", "productImgUrl"] }],
-  });
-
-  return res.status(201).json({
-    status: "success",
-    message: "product created successfully",
-    product: result,
-  });
 });
 
-const removeProduct = catchError(async (req, res) => {
-  const { product } = req;
-  await product.update({ status: "inactive" });
-  return res.json({
-    status: "success",
-    message: "product deleted successfully",
-  });
-});
+Product.addHook("afterUpdate", async (product, options) => {
+  if (product._previousDataValues.status !== product.status) {
+    if (product.status === "pause") {
+      const productCarts = await ProductCart.findAll({
+        where: { productId: product.id },
+      });
 
-const updateProduct = catchError(async (req, res) => {
-  const { product } = req;
-  const { title, description, price, brand, availableQuantity } = req.body;
-
-  await product.update({
-    title: title.toLowerCase().trim(),
-    description: description.toLowerCase().trim(),
-    brand: brand.toLowerCase().trim(),
-    price,
-    availableQuantity,
-  });
-
-  return res.json({
-    status: "success",
-    message: "product updated successfully",
-  });
-});
-
-const updateProductImages = catchError(async (req, res, next) => {
-  const { product, files } = req;
-  const { productImgs } = product;
-
-  const limit = 5;
-
-  if (productImgs.length + files.length > limit)
-    return next(new AppError(`limit of ${limit} images exceeded`, 400));
-
-  const productImgsPromises = files.map(async (file) => {
-    const imgUrl = await UploadFile.uploadToFirebase("products", file);
-    return await ProductImg.create({
-      productId: product.id,
-      productImgUrl: imgUrl,
-    });
-  });
-  await Promise.all(productImgsPromises);
-
-  return res.json({
-    status: "success",
-    message: "product images updated successfully",
-  });
-});
-
-const deleteProductImages = catchError(async (req, res) => {
-  const { productImgs } = req.product;
-  const { deleteProductImgs } = req.body;
-
-  const productImgsPromises = productImgs.map((productImg) => {
-    if (deleteProductImgs.includes(productImg.id)) {
-      return productImg.update({ status: "inactive" });
+      productCarts.map((productCart) => {
+        if (
+          productCart.quantity > product.availableQuantity &&
+          product.availableQuantity > 0
+        ) {
+          productCart.update({ quantity: product.availableQuantity });
+        }
+        productCart.update({ status: "pause" });
+      });
     }
-  });
-  await Promise.all(productImgsPromises);
+    if (product.status === "active") {
+      const productCarts = await ProductCart.findAll({
+        where: { productId: product.id },
+      });
 
-  return res.json({
-    status: "success",
-    message: "product images deleted successfully",
-  });
+      productCarts.map((productCart) => {
+        productCart.update({ status: "active" });
+      });
+    }
+  }
 });
 
-module.exports = {
-  getProducts,
-  getProductsFiltered,
-  createProduct,
-  getProduct,
-  removeProduct,
-  updateProduct,
-  getMyProducts,
-  updateProductImages,
-  deleteProductImages,
-};
+module.exports = Product;
