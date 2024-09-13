7a9e32c46957a1771bbb196af594b1abf8d2521b
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-13 09:30:55 -0600

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,32 +1,35 @@
-const User = require('../models/user.model');
-const AppError = require('../utils/appError');
-const catchError = require('../utils/catchError');
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
+const User = require("../models/user.model");
+const AppError = require("../utils/appError");
+const catchError = require("../utils/catchError");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const protect = catchError(async (req, res, next) => {
   let token;
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
+    req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(' ')[1];
+    token = req.headers.authorization.split(" ")[1];
   }
+  if (!token && req.sessionUserByPass) return next();
+
+  console.log(req.sessionUserByPass);
   if (!token) {
     return next(
-      new AppError('you are not logged in, please log in to get access', 401)
+      new AppError("you are not logged in, please log in to get access", 401)
     );
   }
   const decoded = await promisify(jwt.verify)(token, process.env.TOKEN_SECRET);
 
   const user = await User.findOne({
-    where: { id: decoded.id, status: 'active' },
+    where: { id: decoded.id, status: "active" },
   });
 
   if (!user)
     return next(
-      new AppError('the owner of this token is not longer available', 404)
+      new AppError("the owner of this token is not longer available", 404)
     );
 
   if (user.passwordChangedAt) {
@@ -37,7 +40,7 @@ const protect = catchError(async (req, res, next) => {
 
     if (decoded.iat < changedTimeStamp) {
       return next(
-        new AppError('User recently changed password, please login again', 401)
+        new AppError("User recently changed password, please login again", 401)
       );
     }
   }
@@ -50,9 +53,9 @@ const validPassword = catchError(async (req, res, next) => {
   const { currentPassword, newPassword } = req.body;
 
   if (newPassword === currentPassword)
-    return next(new AppError('the password cannot be equeals', 400));
+    return next(new AppError("the password cannot be equeals", 400));
   const isValid = await verifyPassword(currentPassword, sessionUser.password);
-  if (!isValid) return next(new AppError('incorrect password', 401));
+  if (!isValid) return next(new AppError("incorrect password", 401));
 
   req.user = sessionUser;
   next();
@@ -62,11 +65,16 @@ const restrictTo = (...rols) => {
   return (req, res, next) => {
     if (!rols.includes(req.sessionUser.role)) {
       return next(
-        new AppError('you do not have permission to perform this action', 403)
+        new AppError("you do not have permission to perform this action", 403)
       );
     }
     next();
   };
 };
 
-module.exports = { protect, restrictTo, validPassword };
+const tokenBypass = (req, res, next) => {
+  req.sessionUserByPass = true;
+  next();
+};
+
+module.exports = { protect, restrictTo, validPassword, tokenBypass };
