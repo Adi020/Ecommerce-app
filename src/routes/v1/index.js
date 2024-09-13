02c78bf43620c1ddf2f5fa1d1b824c8789eca675
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-13 09:16:43 -0600

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const userRouter = require('./user.router');
-const authRouter = require('./auth.router');
-const productRouter = require('./product.router');
-const productCartRouter = require('./productCart.router');
-const purchaseRouter = require('./purchase.router');
-const categoryRouter = require('./category.router');
-const ratingRouter = require('./rating.route');
+const express = require("express");
+const userRouter = require("./user.router");
+const authRouter = require("./auth.router");
+const productRouter = require("./product.router");
+const productCartRouter = require("./productCart.router");
+const purchaseRouter = require("./purchase.router");
+const categoryRouter = require("./category.router");
+const ratingRouter = require("./rating.route");
+const productHistoryRouter = require("./history.router");
 
 const router = express.Router();
 
-router.use('/users', userRouter);
-router.use('/auth', authRouter);
-router.use('/products', productRouter);
-router.use('/cart', productCartRouter);
-router.use('/purchases', purchaseRouter);
-router.use('/categories', categoryRouter);
-router.use('/rating', ratingRouter);
+router.use("/users", userRouter);
+router.use("/auth", authRouter);
+router.use("/products", productRouter);
+router.use("/cart", productCartRouter);
+router.use("/purchases", purchaseRouter);
+router.use("/categories", categoryRouter);
+router.use("/rating", ratingRouter);
+router.use("/history", productHistoryRouter);
 
 module.exports = router;
