9ea5f1a53aa9970875efad60c3ee6be334e80101
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-13 05:06:25 -0600

    Update product.model.js

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,8 +1,8 @@
-const { DataTypes } = require('sequelize');
-const { db } = require('../database/config');
-const ProductCart = require('./productCart.model');
+const { DataTypes } = require("sequelize");
+const { db } = require("../database/config");
+const ProductCart = require("./productCart.model");
 
-const Product = db.define('products', {
+const Product = db.define("product", {
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
@@ -40,23 +40,23 @@ const Product = db.define('products', {
     defaultValue: 0,
   },
   status: {
-    type: DataTypes.ENUM('active', 'inactive', 'pause'),
+    type: DataTypes.ENUM("active", "inactive", "pause"),
     allowNull: false,
-    defaultValue: 'active',
+    defaultValue: "active",
   },
 });
 
-Product.addHook('beforeSave', async (product, options) => {
+Product.addHook("beforeSave", async (product, options) => {
   if (product.availableQuantity === 0) {
-    product.status = 'pause';
-  } else if (product.status === 'pause') {
-    product.status = 'active';
+    product.status = "pause";
+  } else if (product.status === "pause") {
+    product.status = "active";
   }
 });
 
-Product.addHook('afterUpdate', async (product, options) => {
+Product.addHook("afterUpdate", async (product, options) => {
   if (product._previousDataValues.status !== product.status) {
-    if (product.status === 'pause') {
+    if (product.status === "pause") {
       const productCarts = await ProductCart.findAll({
         where: { productId: product.id },
       });
@@ -68,16 +68,16 @@ Product.addHook('afterUpdate', async (product, options) => {
         ) {
           productCart.update({ quantity: product.availableQuantity });
         }
-        productCart.update({ status: 'pause' });
+        productCart.update({ status: "pause" });
       });
     }
-    if (product.status === 'active') {
+    if (product.status === "active") {
       const productCarts = await ProductCart.findAll({
         where: { productId: product.id },
       });
 
       productCarts.map((productCart) => {
-        productCart.update({ status: 'active' });
+        productCart.update({ status: "active" });
       });
     }
   }
