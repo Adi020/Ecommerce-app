86ca78be1ae0f6e320405bcbade56f951f867066
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-18 11:41:08 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
-const express = require('express');
-require('./models');
+const express = require("express");
+require("./models");
 
-const cors = require('cors');
-const { default: rateLimit } = require('express-rate-limit');
-const { default: helmet } = require('helmet');
-const hpp = require('hpp');
-const { xss } = require('express-xss-sanitizer');
+const cors = require("cors");
+const { default: helmet } = require("helmet");
+const hpp = require("hpp");
+const { xss } = require("express-xss-sanitizer");
 
-const globalErrorHandler = require('./middlewares/error.middleware');
-const { routeNotFound } = require('./controllers/v1/all.controller');
+const globalErrorHandler = require("./middlewares/error.middleware");
+const { routeNotFound } = require("./controllers/v1/all.controller");
 
-const v1Router = require('./routes/v1');
+const v1Router = require("./routes/v1");
+const limitRequest = require("./utils/rateLimit");
 
 const app = express();
-const limiter = rateLimit({
-  max: 1000,
-  windowMs: 60 * 60 * 1000,
-  message: 'too many request from this ip, please try again in an hour',
-});
 
-app.use(express.json({ limit: '4kb' }));
+const limiter = limitRequest(1000, 60 * 60 * 1000, "too many request from this ip, please try again in an hour");
+
+app.use(express.json({ limit: "4kb" }));
 app.use(cors());
 app.use(helmet());
 app.use(hpp());
@@ -27,8 +24,8 @@ app.use(xss());
 
 app.use(limiter);
 
-app.use('/api/v1', v1Router);
-app.all('*', routeNotFound);
+app.use("/api/v1", v1Router);
+app.all("*", routeNotFound);
 
 app.use(globalErrorHandler);
 
