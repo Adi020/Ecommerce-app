eaa1dfc886e486039f20b939ddb2d246e9716f42
Adi <121740959+Adi020@users.noreply.github.com>
2024-09-20 04:14:13 -0600

    Update product.router.js

--- src/routes/v1/product.router.js
+++ src/routes/v1/product.router.js
@@ -8,6 +8,7 @@ const {
   updateProductImages,
   deleteProductImages,
   getProductsFiltered,
+  getProductTest,
 } = require("../../controllers/v1/product.controller");
 const express = require("express");
 const upload = require("../../utils/multer");
@@ -25,6 +26,7 @@ const {
   dataProductValidation,
   deleteProductImagesValidation,
   filterProducstValidation,
+  validationId,
 } = require("../../middlewares/validations.middleware");
 
 const productRouter = express.Router();
@@ -45,18 +47,19 @@ productRouter
 productRouter.route("/me").get(protect, getMyProducts);
 
 productRouter
-  .use("/me/:id", protect, validProductUser)
+  .use("/me/:id", validationId, protect, validProductUser)
   .route("/me/:id")
   .delete(removeProduct)
   .patch(dataProductValidation, updateProduct);
 
 productRouter
-  .use("/me/:id/images", protect, validProductUser)
+  .use("/me/:id/images", validationId, protect, validProductUser)
   .route("/me/:id/images")
   .patch(upload.array("productImgs", 5), updateProductImages)
   .delete(deleteProductImagesValidation, deleteProductImages);
 
 productRouter
+  .use("/:id", validationId)
   .route("/:id")
   .get(tokenBypass, validProductPerFindOne, getProduct)
   .delete(protect, restrictTo("admin"), validProductAdmin, removeProduct)
